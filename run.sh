35f031e37335f3b1dc6b1182957eae6027fa912c
voutasaurus <voutasaurus@gmail.com>
2022-12-01 14:54:39 +0800

    add checksum logic

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
 echo "running script"
+
+git clone https://bitbucket.org/modus/analysis-nomenclature.git
+
+if [[ $(shasum --strict -a 256 -c project/checksum.txt 2>&1) == *FAIL* ]]
+then
+	"checksum failed, file updated"
+	exit 1
+fi
